7c6bbbfedbdd168c3e42a4265585de734302a883
cuong3004 <duongduccuong30042002@gmail.com>
2022-12-21 07:15:14 +0700

    update

--- student_mod/dataset.py
+++ student_mod/dataset.py
@@ -13,24 +13,6 @@ from PIL.Image import Resampling
 import pytorch_lightning as pl
 import numpy as np
 
-
-# collate_fn = lightly.data.SimCLRCollateFunction(
-#     input_size=img_size
-# )
-
-# dataset_train_moco = lightly.data.LightlyDataset(
-#     input_dir=path_to_train,
-# )
-
-# dataloader_train_moco = torch.utils.data.DataLoader(
-#     dataset_train_moco,
-#     batch_size=batch_size,
-#     shuffle=True,
-#     collate_fn=collate_fn,
-#     drop_last=True,
-#     num_workers=num_workers
-# )
-
 class CelebAStudentDataModule(pl.LightningDataModule):
     def __init__(self, 
         data_train_loader,
